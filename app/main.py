2f83eaadfaa7be3efc8dd138ce88d4272e6361d4
Krzysztof Kołtowski <kadis@o2.pl>
2025-07-29 08:47:28 +0200

    Solution 1.2

--- app/main.py
+++ app/main.py
@@ -15,7 +15,11 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     @classmethod
     def __str__(cls) -> str:
